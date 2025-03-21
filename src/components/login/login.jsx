516b904f7b921fe560865c046a980b2f834f1417
Aquib <aquibhingwala@gmail.com>
2025-03-21 11:12:07 +0530

    wdq

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -38,7 +38,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://consultancy-api.code4bharat.com/api/auth/login",
+        "https://consultancy-api.code4bharat.com/api/auth/login",
         { email: formData.email, password: formData.password },
         { headers: { "Content-Type": "application/json" }, withCredentials: true }
       );
@@ -80,7 +80,7 @@ export default function Login() {
 
     try {
       await axios.post(
-        "http://consultancy-api.code4bharat.com/api/auth/register",
+        "https://consultancy-api.code4bharat.com/api/auth/register",
         {
           name: formData.name,
           email: formData.email,
